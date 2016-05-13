bc384c411b2a0386a8aa8c19fdeb6971885da219
Calin Don <calin.don@gmail.com>
2016-05-13 16:03:14 +0300

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for private_key in $(find /secrets -type f -name '*.gpg') ; do
+    if [ -f "$private_key.passpharase" ] ; then
+        cat "$private_key" | gpg --import --passphrase-file "$private_key.passpharase"
+    else
+        cat "$private_key" | gpg --import
+    fi
+done
+
+for d in /source/* ; do
+    if [ -d "$d" ] ; then
+        (
+            set -x
+            cd "$d"
+            mk-build-deps --install --remove --tool "apt-get --no-install-recommends --yes"
+            debuild $DEBUILD_OPTS --lintian-opts $LINTIAN_OPTS --allow-root
+        )
+    fi
+done
