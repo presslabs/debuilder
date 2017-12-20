0f9b04e5683e8486f9c72c2c69a4bec8ca372513
Calin Don <calin.don@gmail.com>
2017-12-20 17:02:52 +0200

    Import GPG key from env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -z "$PGP_KEY" ] ; then
+    echo "$PGP_KEY" | gpg --import
+fi
+
 for private_key in $(find /secrets -type f -name '*.gpg') ; do
     if [ -f "$private_key.passpharase" ] ; then
         cat "$private_key" | gpg --import --passphrase-file "$private_key.passpharase"
